aeba46792f76ce3a2b94d494e09258344d534e9a
Shiram Kshettri <shiram.kshettri@gmail.com>
2017-05-28 14:29:58 +0200

    studentliv side

--- studentliv.php
+++ studentliv.php
@@ -14,21 +14,33 @@ $utesteders = Utesteder::limit(3)->get();
 
 ?>
 
-<?php require 'header.php' ?>
+    <?php require 'header.php' ?>
 
-<main id="studentliv_main">
-    <section> <img src="images/studentliv/student_banner.png" </section>
-    <section class="studentliv_kategori">
-        <div> <img src="images/studentliv/student1.png"> <header> </header> </div>
-        <div> <header> <p>Westerdals Oslo ACT has 1700 students enrolled in 25 different study programmes. We have talked to some of the students about their experience.</p></div>
-        <div> <img src="images/studentliv/student2.png"> <header></div>
-     </section>
-   </main>
-<script>
-   $("#lnk_spise").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#spise_section").offset().top-87
-    }, 2000);
-});
-</script>
-<?php require 'footer.php' ?>
+    <main id="studentliv_main">
+        <section> <img src="images/studentliv/student_banner.png" </section>
+            <section class="studentliv_kategori">
+                <div> <img src="images/studentliv/student1.png">
+                    <header> </header>
+                </div>
+                <div>
+                    <p>Hvert år er 1700 studenter innskrevet i Westerdals Oslo ACT i 25 forskjellige programmer. Hver student har ulike behov og interesser, og her kan du finne nyttige lenker for studentlivet ditt.</p>
+                </div>
+                <div> <img src="images/studentliv/student2.png">
+                    <header>
+                </div>
+            </section>
+            <section class="studentliv_link">
+                <header> Noen nyttige linker</header>
+                <div> <img src="images/studentliv/sio.jpeg"><header> <a href="https://www.sio.no/en/" target="_blank"> SIO</a> 
+                    </header>
+                    <p>SiO er studentsamskipnaden i Oslo og Akershus, hjelper med ulike områder som bolig, treningssenter, barnehage etc. Alle fordeler for sio medlemmer er tilgjengelige hvis du er en aktiv student og betaler semesteravgift.</p>
+                </div>
+    
+                <div> <img src="images/studentliv/student_torget.jpeg">
+                     <header> <a href="http://studenttorget.no/" target="_blank">Student Torget</a></header>
+                    
+                </div>
+            </section>
+
+    </main>
+    <?php require 'footer.php' ?>
